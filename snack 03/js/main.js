13b7d9d70f21a1a7c7fdfaf3e29ca1296d020e6d
HikariNM <nicole.hikari.mascellino@gmail.com>
2025-11-22 09:58:14 +0100

    creato array snack 03

---
+++ snack 03/js/main.js
@@ -0,0 +1,9 @@
+const animals = [
+    {nome: 'Leone', famiglia: 'Felidi', classe: 'Mammiferi',},
+    {nome: 'Squalo', famiglia: 'lamnidi', classe: 'pesci',},
+    {nome: 'Aquila', famiglia: 'Accipitridi' , classe: 'Uccelli',},
+    {nome: 'Cane', famiglia: 'Canidi', classe: 'Mammiferi',},
+    {nome: 'Rana', famiglia: 'Ranidi', classe: 'Anfibi' },
+    {nome: 'Elefante', famiglia: 'Elefantidi', classe: 'Mammiferi' },
+    {nome: 'Pinguino', famiglia: 'Sfeniscidi', classe: 'Uccelli' },
+]
