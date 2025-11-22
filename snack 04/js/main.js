88454c419441e7cb45c782ed5896e329404935fd
HikariNM <nicole.hikari.mascellino@gmail.com>
2025-11-22 10:28:28 +0100

    snack 04 versione compatta

--- snack 04/js/main.js
+++ snack 04/js/main.js
@@ -4,11 +4,17 @@ const person = [
     { nome: 'Michele', cognome: 'DeLuca', eta: 25, },
 ]
 
-const newArray = person.map((element) => {
+/********************************************************************************************************** */
+const driveMessage = person.map((element) => {
     if (element.eta > 18) {
         return `${element.nome} ${element.cognome} può guidare perchè ha ${element.eta}`;
     }
     else {
-    return `${element.nome} ${element.cognome} non può guidare perchè ha ${element.eta}`;}
-});
+        return `${element.nome} ${element.cognome} non può guidare perchè ha ${element.eta}`;}
+    });
+    console.log(driveMessage)
+    
+/*****************************************VERSIONE COMPATTA************************************************ */
+
+const newArray = person.map(element => element.eta > 18 ? `${element.nome} ${element.cognome} può guidare perchè ha ${element.eta}` : `${element.nome} ${element.cognome} non può guidare perchè ha ${element.eta}`)
 console.log(newArray)
