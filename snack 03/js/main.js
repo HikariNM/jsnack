616452de3e054154a4ad6c25f01b58470f3f18d5
HikariNM <nicole.hikari.mascellino@gmail.com>
2025-11-22 10:04:05 +0100

    snack 03

--- snack 03/js/main.js
+++ snack 03/js/main.js
@@ -1,9 +1,12 @@
 const animals = [
     {nome: 'Leone', famiglia: 'Felidi', classe: 'Mammiferi',},
-    {nome: 'Squalo', famiglia: 'lamnidi', classe: 'pesci',},
+    {nome: 'Squalo', famiglia: 'Lamnidi', classe: 'Pesci',},
     {nome: 'Aquila', famiglia: 'Accipitridi' , classe: 'Uccelli',},
     {nome: 'Cane', famiglia: 'Canidi', classe: 'Mammiferi',},
     {nome: 'Rana', famiglia: 'Ranidi', classe: 'Anfibi' },
     {nome: 'Elefante', famiglia: 'Elefantidi', classe: 'Mammiferi' },
     {nome: 'Pinguino', famiglia: 'Sfeniscidi', classe: 'Uccelli' },
 ]
+
+const mammals = animals.filter(animal => animal.classe === 'Mammiferi');
+console.log(mammals)
