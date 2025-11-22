8e0b864253817472325abbdb4e7ab4640ca03412
HikariNM <nicole.hikari.mascellino@gmail.com>
2025-11-22 09:20:55 +0100

    creato array snack 01

---
+++ snack 01/js/main.js
@@ -0,0 +1,12 @@
+const veicle = [
+    {marca: 'Fiat', modello: 'Panda', alimentazione: 'GPL',},
+    {marca: 'Audi', modello: 'A3', alimentazione: 'Benzina',},
+    {marca: 'Renault', modello: 'Clio', alimentazione: 'Diesel',},
+    {marca: 'Dacia', modello: 'Duster', alimentazione: 'Elettrico',},
+    {marca: 'Volkswagen', modello: 'Polo', alimentazione: 'Diesel',},
+    {marca: 'Toyota', modello: 'Corolla', alimentazione: 'GPL',},
+    {marca: 'Ford', modello: 'Focus', alimentazione: 'Metano',},
+    {marca: 'BMW', modello: 'X6', alimentazione: 'Benzina',},
+    {marca: 'Mercedes', modello: 'EQA', alimentazione: 'Elettrico',},
+    {marca: 'Seat', modello: 'Ibiza', alimentazione: 'Diesel',},
+]
