3a9ad4e0278d0c31e97ad0fe420980b1c37c6551
HikariNM <nicole.hikari.mascellino@gmail.com>
2025-11-22 10:18:11 +0100

    snack 04

--- snack 04/js/main.js
+++ snack 04/js/main.js
@@ -1,5 +1,14 @@
 const person = [
-    {nome:'Mario', cognome:'Rossi', eta: 35,},
-    {nome:'Sara', cognome:'Conti', eta: 16,},
-    {nome:'Michele', cognome:'DeLuca', eta: 25,},
+    { nome: 'Mario', cognome: 'Rossi', eta: 35, },
+    { nome: 'Sara', cognome: 'Conti', eta: 16, },
+    { nome: 'Michele', cognome: 'DeLuca', eta: 25, },
 ]
+
+const newArray = person.map((element) => {
+    if (element.eta > 18) {
+        return `${element.nome} ${element.cognome} può guidare perchè ha ${element.eta}`;
+    }
+    else {
+    return `${element.nome} ${element.cognome} non può guidare perchè ha ${element.eta}`;}
+});
+console.log(newArray)
